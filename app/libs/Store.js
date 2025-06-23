f8702425245add8fe83de5a796b7502aec94516b
AWAIS_SUFI\awais <awais6javaid@gmail.com>
2025-06-23 11:07:02 +0500

    PR-leraning

---
+++ app/libs/Store.js
@@ -0,0 +1,9 @@
+// lib/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
---
+++ app/page.jsx
@@ -0,0 +1,43 @@
+// app/page.jsx
+"use client";
+
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  multiply,
+  reset,
+} from "./libs/features/counterSlice";
+
+export default function HomePage() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="text-2xl text-blue-500">
+      welcome to Learn Redux toolkit
+      <div style={{ padding: "1rem" }}>
+        <button
+          onClick={() => dispatch(increment())}
+          style={{ marginRight: "1rem" }}
+        >
+          + Increment
+        </button>
+        <h1>Count: {count}</h1>
+        <button onClick={() => dispatch(decrement())}>- Decrement</button>
+        <br />
+        <button onClick={() => dispatch(multiply())}>* multiply</button>
+      </div>
+      <button
+        onClick={() => dispatch(reset())}
+        style={{
+          backgroundColor: "#f87171",
+          color: "white",
+          marginLeft: "1rem",
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
+}
