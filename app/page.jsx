0936fd0874226499ee09be3c7c9851f5163e4d34
AWAIS_SUFI\awais <awais6javaid@gmail.com>
2025-06-23 16:52:14 +0500

    design

--- app/page.jsx
+++ app/page.jsx
@@ -14,31 +14,41 @@ export default function HomePage() {
   const dispatch = useDispatch();
 
   return (
-    <div className="text-2xl text-center text-blue-500">
-      <h1>Leran Also Pull-Request</h1>
-      welcome to Learn Redux toolkit
-      <div style={{ padding: "1rem" }}>
+    <>
+      <div className="text-2xl text-center text-blue-500">
+        <h1 className="text-2xl text-lime-500 py-5">Leran Also Pull-Request</h1>
+        welcome to Learn Redux toolkit
+        <div className="flex justify-center items-center py-5">
+          <button
+            className="bg-amber-600 rounded-3xl px-5 py-3"
+            onClick={() => dispatch(decrement())}
+          >
+            <span className="text-white">Decrement -</span>
+          </button>
+          <h1 className="px-5">Count: {count}</h1>
+          <button
+            onClick={() => dispatch(increment())}
+            className="bg-amber-600 rounded-3xl px-5 py-3"
+          >
+            <span className="text-white">increment +</span>
+          </button>
+          <br />
+        </div>
         <button
-          onClick={() => dispatch(increment())}
-          style={{ marginRight: "1rem" }}
+          className="bg-amber-600 rounded-3xl px-5 py-3"
+          onClick={() => dispatch(multiply())}
         >
-          + Increment
+          <span className="text-white">multiply *</span>
         </button>
-        <h1>Count: {count}</h1>
-        <button onClick={() => dispatch(decrement())}>- Decrement</button>
-        <br />
-        <button onClick={() => dispatch(multiply())}>* multiply</button>
       </div>
-      <button
-        onClick={() => dispatch(reset())}
-        style={{
-          backgroundColor: "#f87171",
-          color: "white",
-          marginLeft: "1rem",
-        }}
-      >
-        Reset
-      </button>
-    </div>
+      <div className="flex items-center justify-center mt-5">
+        <button
+          className="bg-red-600  rounded-3xl px-5 py-3"
+          onClick={() => dispatch(reset())}
+        >
+          <span className="text-white text-2xl">Reset</span>
+        </button>
+      </div>
+    </>
   );
 }
